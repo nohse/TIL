455fa0826e78ae4d39fee4b2228a141d21313c81
nohse <watercar67@naver.com>
2023-09-07 10:25:39 +0900

    자바 프로그래밍 공부

---
+++ Java/1주차/JC20011619Q01.java
@@ -0,0 +1,22 @@
+
+public class JC20011619Q01 {
+	
+	public static int sum(int n, int m) { 
+		return n + m;
+	}
+	
+	// main() 메소드에서 실행 시작
+	public static void main(String[] args) { 
+		int i = 20;
+		int s;
+		char a;
+
+		s = sum(i, 10); // sum() 메소드 호출
+		a = '?';
+		System.out.println("20011619노성현");//첫번째로 이름과 학번 출력
+		System.out.println(a); // 문자 '?' 화면 출력
+		System.out.println("Hello"); // "Hello" 문자열 화면 출력
+		System.out.println(s); // 정수 s 값 화면 출력
+	}
+
+}
