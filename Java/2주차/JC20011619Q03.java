ebd982b9ba8541fdb8db88ef83f9968b95b55e16
nohse <watercar67@naver.com>
2023-09-12 10:36:32 +0900

    2주차 JAVA 실습문제

---
+++ Java/2주차/JC20011619Q03.java
@@ -0,0 +1,11 @@
+public class JC20011619Q03 {
+	public static void main (String[] args) {
+		System.out.println("노성현20011619");
+		int a = 3, b = 5;
+		if(a>b)
+			System.out.println("두 수의 차는 " + (a-b));
+		else
+			System.out.println("두 수의 차는 " + (b-a));
+	}
+
+}
