bc953740861a9dc944482b40c1755fae91e9f48d
nohse <watercar67@naver.com>
2023-12-14 00:40:59 +0900

    소스 코드 이름 변경

--- Java/예제문제/Java13_05.java
+++ Java/예제문제/Java13_05.java
@@ -2,73 +2,60 @@ package Num1;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.*;
-class RandomThread extends Thread{
-	private Container c;
-	private boolean finish=false;
-	public RandomThread(Container c) {
-		this.c=c;
-	}
-	void finish() {
-		finish=true;
+class TimerRunnable1 extends Thread{
+	private JLabel la=new JLabel();
+	public TimerRunnable1(JLabel la) {
+		this.la=la;
 	}
 	public void run() {
+		int n=0;
 		while(true) {
-			int x = ((int)(Math.random()*c.getWidth()));
-			int y = ((int)(Math.random()*c.getHeight()));
-			JLabel label = new JLabel("Java"); //새 레이블 생성
-			label.setSize(80, 30); 
-			label.setLocation(x, y);		
-			c.add(label);
-			c.repaint(); 
-
+			la.setText(Integer.toString(n));
+			n++;
 			try {
-				sleep(300);
-				if(finish==true) {
-					c.removeAll();
-					label = new JLabel("finish"); 
-					label.setSize(80, 30); 
-					label.setLocation(100, 100); 
-					label.setForeground(Color.RED);
-					c.add(label);
-					c.repaint();
-					return;
-				}
+			sleep(1000);
 			}catch(InterruptedException e) {
 				return ;
 			}
+
 		}
 	}
 }
-class ThreadFinishFlagEx extends JFrame{
-	private RandomThread th;
-	ThreadFinishFlagEx(){
+class ThreadInterruptEx1 extends JFrame{
+	JButton btn=new JButton("kill timer");
+	JLabel la=new JLabel();
+	TimerRunnable1 th=new TimerRunnable1(la);
+	public ThreadInterruptEx1() {
 		setTitle("20011619nohseonghyeon");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c=getContentPane();
-		c.setLayout(null);
-		
-		c.addMouseListener(new MouseAdapter() {
+		Container c= getContentPane();
+		c.setLayout(new FlowLayout());
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-				th.finish();
-			}
-		});
-		setSize(300,200);
+		btn.addActionListener(new MyActionListener());
+		c.add(la);
+		c.add(btn);
+		
+		setSize(500,250);
 		setVisible(true);
 		
-		th=new RandomThread(c);
 		th.start();
+		
+	}
+	class MyActionListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			th.interrupt();
+			JButton btn=(JButton)e.getSource();
+			btn.setEnabled(false);
+		}
 	}
 }
-public class Java13_05 {
+public class Java13_04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new ThreadFinishFlagEx();
+		new ThreadInterruptEx1();
 	}
 }
